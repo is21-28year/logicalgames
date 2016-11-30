093e23da7e9814c30f7653050df66e7f9dfe63df
kotaseto <150012@yses.ac.jp>
2016-11-30 14:28:38 +0900

    わらしべ
    難易度別分け
    cssは途中

--- javascript/warashibe_Question.js
+++ javascript/warashibe_Question.js
@@ -1,43 +1,64 @@
 //初期設定
-//場所:0~7
-var place = [0,1,2,3,4,5,6,7];
+var myitem;
+var maxloop;
+var place;
+var done;
+var haveitem;
+var wantitem;
+var gitem;
+
 function shoki(mode){
     //自分が持っているアイテム
-    var myitem = 0;
-    //交換済フラグ
-    var done = [0,0,0,0,0,0,0,0];
+    myitem = 0;
     if(mode == "easy"){
+        maxloop = 3;
+        //場所:0~7
+        place = [0,1,2,3];
+        //交換済フラグ
+        done = [0,0,0,0];
         //キャラクターが持っているアイテム
-        //0~7
-        var haveitem = [1,2,0,3,4,2,6,7];
+        //0~3
+        haveitem = [1,2,3,0];
         //キャラクターが欲しがっているアイテム
-        //0~7
-        var wantitem =[0,0,2,2,2,4,4,4];
+        //0~3
+        wantitem =[0,1,2,2];
         //目標のアイテム
-        var gitem = 7;
+        gitem = 3;
     }
     if(mode == "normal"){
+        maxloop = 5;
+        //場所:0~7
+        place = [0,1,2,3,4,5];
+        //交換済フラグ
+        done = [0,0,0,0,0,0];
         //キャラクターが持っているアイテム
-        //0~7
-        var haveitem = [1,2,0,3,4,2,6,7];
+        //0~5
+        haveitem = [1,2,3,4,5,2];
         //キャラクターが欲しがっているアイテム
-        //0~7
-        var wantitem =[0,0,2,2,2,4,4,4];
+        //0~5
+        wantitem =[0,0,2,3,3,4];
         //目標のアイテム
-        var gitem = 7;
+        gitem = 5;
     }
     if(mode == "hard"){
+        maxloop = 7;
+        //場所:0~7
+        place = [0,1,2,3,4,5,6,7];
+        //交換済フラグ
+        done = [0,0,0,0,0,0,0,0];
         //キャラクターが持っているアイテム
         //0~7
-        var haveitem = [1,2,0,3,4,2,6,7];
+        haveitem = [1,2,0,3,4,2,6,7];
         //キャラクターが欲しがっているアイテム
         //0~7
-        var wantitem =[0,0,2,2,2,4,4,4];
+        wantitem =[0,0,2,2,2,4,4,4];
         //目標のアイテム
-        var gitem = 7;
+        gitem = 7;
     }
     
 }
+/*
+var place = [0,1,2,3,4,5,6,7];
 //キャラクターが持っているアイテム
 //0~7
 var haveitem = [1,2,0,3,4,2,6,7];
@@ -47,13 +68,14 @@ var wantitem =[0,0,2,2,2,4,4,4];
 //※場所、haveアイテム、wantアイテムは常に同じ組み合わせになる
 //交換済フラグ
 var done = [0,0,0,0,0,0,0,0];
-//表示する画像
-//拡張子抜きの画像名が入る(png)
-var img = ["burger","chicken","momo","sakana","ame","kani","oniku","orange"];
 //自分が持っているアイテム
 var myitem = 0;
 //目標のアイテム
 var gitem = 7;
+*/
+//表示する画像
+//拡張子抜きの画像名が入る(png)
+var img = ["burger","chicken","momo","sakana","ame","kani","oniku","orange"];
 //キャラの画像
 //拡張子抜きの画像名が入る(png)
 var chrimg = ["cat","dog","gagi","girl","okan","ossan","shadow","work"];
@@ -64,7 +86,7 @@ var tmpB;
 //シャッフル処理
 function shuffle(){
     //位置のシャッフル
-    for(var k=7;k>0;k--){
+    for(var k=maxloop;k>0;k--){
         //0からkまでのK+1個から１つ選択
         var t = Math.floor( Math.random() * (k+1) ) ;
         //選択された要素をk番目の要素と入れ替える
@@ -79,7 +101,7 @@ function shuffle(){
         wantitem[k] = tmpA;
     }
     //アイテム画像のシャッフル
-    for(var l=7;l>0;l--){
+    for(var l=maxloop;l>0;l--){
         //0からlまでのl+1個から１つ選択
         var m = Math.floor( Math.random() * (l+1) ) ;
         //選択された要素をl番目の要素と入れ替える
@@ -88,7 +110,7 @@ function shuffle(){
         img[l] = tmpB;
     }
     //キャラクター画像のシャッフル
-    for(var l=7;l>0;l--){
+    for(var l=maxloop;l>0;l--){
         //0からlまでのl+1個から１つ選択
         var m = Math.floor( Math.random() * (l+1) ) ;
         //選択された要素をl番目の要素と入れ替える
@@ -119,7 +141,7 @@ function koukan(disp){
             }else{
                 //ゲームオーバー判定
                 var goflag = 1;
-                for(var s=0;s<8;s++){
+                for(var s=0;s<(maxloop+1);s++){
                     //自分のアイテムを欲しがっている人がいる かつ その人物が交換済みでないなら
                     if(myitem == wantitem[s] && done[s] != 1){
                         //ゲームオーバーフラグをオフ
@@ -136,11 +158,14 @@ function koukan(disp){
 }
 //表示更新処理
 function renewal(){
-    //自分が持っているアイテム
-    document.getElementById("myitem").innerHTML="<img src='../image/"+img[myitem]+".png'>";
     //目標のアイテム
     document.getElementById("gitem").innerHTML="<img src='../image/"+img[gitem]+".png'>";
-    for(var i=0;i<8;i++){
+    document.getElementById("gitem").innerHTML+="ほしいもの";
+    //自分が持っているアイテム
+    document.getElementById("myitem").innerHTML="<img src='../image/"+img[myitem]+".png'>";
+    document.getElementById("myitem").innerHTML+="もちもの";
+    
+    for(var i=0;i<(maxloop+1);i++){
         //キャラクターが現在持っているアイテム
         
         //document.getElementById("c"+i).innerHTML+=haveitem[i];
@@ -151,16 +176,16 @@ function renewal(){
             //document.getElementById("c"+i).innerHTML+="<img src='../image/"+img[wantitem[i]]+".png'>";
             //document.getElementById("c"+i).innerHTML+=wantitem[i];
         }else{
-            document.getElementById("c"+i).innerHTML="<div class='item'><img class='chitem' src='../image/"+img[haveitem[i]]+".png'></div>";
+            document.getElementById("c"+i).innerHTML="<div class='item'><img class='cgitem' src='../image/"+img[haveitem[i]]+".png'></div>";
         }
         //場所
         //document.getElementById("c"+i).innerHTML+="<br>場所";
         //document.getElementById("c"+i).innerHTML+=place[i];
-        document.getElementById("c"+i).innerHTML+="";
         if(done[i] == 0){
-            document.getElementById("c"+i).innerHTML+="<br><img class='chrimg' src='../image/"+chrimg[i]+"_1.png'>";
+            document.getElementById("c"+i).innerHTML+="<img class='arrow' src='../image/hidari.png'>";
+            document.getElementById("c"+i).innerHTML+="<img class='chrimg' src='../image/"+chrimg[i]+"_1.png'>";
         }else{
-            document.getElementById("c"+i).innerHTML+="<br><img class='chrimg' src='../image/"+chrimg[i]+"_2.png'>";
+            document.getElementById("c"+i).innerHTML+="<img class='chrimg' src='../image/"+chrimg[i]+"_2.png'>";
         }
     }
 }
