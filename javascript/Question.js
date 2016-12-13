4896c1ffec566b7c173d96a47f62d9685ddba702
kotaseto <150012@yses.ac.jp>
2016-12-13 10:26:48 +0900

    途中までだけど

--- javascript/Question.js
+++ javascript/Question.js
@@ -1,24 +1,99 @@
+/*クリアフラグ三つ毎*/ 
+function cleartrue(clearnum){
+    window.localStorage.setItem(clearnum,true);
+}
 //モーダルダイアログの表示
-function modal(modal_content){
-    //モーダルダイアログに表示させる内容
-    switch (modal_content){
-        case 0:
-            document.getElementById("modal-content").innerHTML="<p>問題文</p>";
-            document.getElementById("modal-content").innerHTML+="<input type='button' id='modal-close' value='閉じる'>";
-            break;
-        case 1:
-            document.getElementById("modal-content").innerHTML="<p>ヒント</p>";
-            document.getElementById("modal-content").innerHTML+="<input type='button' id='modal-close' value='閉じる'>";
-            break;
-        case 2:
-            document.getElementById("modal-content").innerHTML="<p>ゲームクリア</p>";
-            document.getElementById("modal-content").innerHTML+="<input type='button' id='modal-close' value='閉じる'>";
-            break;
-        case 3:
-            document.getElementById("modal-content").innerHTML="<p>ゲームオーバー</p>";
-            document.getElementById("modal-content").innerHTML+="<input type='button' id='modal-close' value='閉じる'>";
-    }
+function modal(mc){
     $(function(){
+
+    //モーダルウィンドウを出現させるクリックイベント
+    $(function(){
+
+        //キーボード操作などにより、オーバーレイが多重起動するのを防止する
+        $( this ).blur() ;	//ボタンからフォーカスを外す
+        if( $( "#modal-overlay" )[0] ) return false ;		//新しくモーダルウィンドウを起動しない (防止策1)
+        //if($("#modal-overlay")[0]) $("#modal-overlay").remove() ;		//現在のモーダルウィンドウを削除して新しく起動する (防止策2)
+
+        //オーバーレイを出現させる
+        $( "body" ).append( '<div id="modal-overlay"></div>' ) ;
+        $( "#modal-overlay" ).fadeIn( "slow" ) ;
+
+        //コンテンツをセンタリングする
+        centeringModalSyncer() ;
+        alert(mc);
+        //コンテンツをフェードインする
+        $( "#modal-content"+mc ).fadeIn( "slow" ) ;
+        if(mc == 3){
+            //クリックしても閉じない
+            $( "#modal-close" ).unbind().click( function(){
+                //[#modal-content]と[#modal-overlay]をフェードアウトした後に…
+                $( "#modal-overlay,#modal-content3" ).fadeOut( "slow" , function(){
+
+                    //[#modal-overlay]を削除する
+                    $('#modal-overlay').remove() ;
+                } ) ;
+
+            } ) ;
+        }else{
+            //[#modal-overlay]、または[#modal-close]をクリックしたら…
+            $( "#modal-overlay,#modal-close" ).unbind().click( function(){
+                alert(mc);
+                //[#modal-content]と[#modal-overlay]をフェードアウトした後に…
+                $( "#modal-overlay,#modal-content"+mc ).fadeOut( "slow" , function(){
+
+                    //[#modal-overlay]を削除する
+                    $('#modal-overlay').remove() ;
+                } ) ;
+
+            } ) ;
+        }
+
+    } ) ;
+    
+
+    //リサイズされたら、センタリングをする関数[centeringModalSyncer()]を実行する
+    $( window ).resize( centeringModalSyncer ) ;
+
+        //センタリングを実行する関数
+        function centeringModalSyncer() {
+
+            //画面(ウィンドウ)の幅、高さを取得
+            var w = $( window ).width() ;
+            var h = $( window ).height() ;
+
+            // コンテンツ(#modal-content)の幅、高さを取得
+            // jQueryのバージョンによっては、引数[{margin:true}]を指定した時、不具合を起こします。
+    //		var cw = $( "#modal-content" ).outerWidth( {margin:true} );
+    //		var ch = $( "#modal-content" ).outerHeight( {margin:true} );
+            var cw = $( "#modal-content"+mc ).outerWidth();
+            var ch = $( "#modal-content"+mc ).outerHeight();
+
+            //センタリングを実行する
+            $( "#modal-content"+mc ).css( {"left": ((w - cw)/2) + "px","top": ((h - ch)/2) + "px"} ) ;
+
+        }
+
+    } ) ;
+}
+
+function mapjump(){
+    window.location.href = 'map.html';
+}
+
+/*
+function  localput(){
+    //回転クリアフラグput
+    window.localStorage.setItem("kaiten_3",true); 
+}
+*/
+/*画面の縦横判定共通部分前頁共通*/
+function viewhantei(){
+
+    $(function(){
+		if($(window).height()<$(window).width()){
+			
+		}else{
+			 $(function(){
 
     //モーダルウィンドウを出現させるクリックイベント
     $(function(){
@@ -36,19 +111,7 @@ function modal(modal_content){
         centeringModalSyncer() ;
 
         //コンテンツをフェードインする
-        $( "#modal-content" ).fadeIn( "slow" ) ;
-
-        //[#modal-overlay]、または[#modal-close]をクリックしたら…
-        $( "#modal-overlay,#modal-close" ).unbind().click( function(){
-
-            //[#modal-content]と[#modal-overlay]をフェードアウトした後に…
-            $( "#modal-content,#modal-overlay" ).fadeOut( "slow" , function(){
-
-                //[#modal-overlay]を削除する
-                $('#modal-overlay').remove() ;
-            } ) ;
-
-        } ) ;
+        $( "#viewchange" ).fadeIn( "slow" ) ;
 
     } ) ;
 
@@ -66,22 +129,30 @@ function modal(modal_content){
             // jQueryのバージョンによっては、引数[{margin:true}]を指定した時、不具合を起こします。
     //		var cw = $( "#modal-content" ).outerWidth( {margin:true} );
     //		var ch = $( "#modal-content" ).outerHeight( {margin:true} );
-            var cw = $( "#modal-content" ).outerWidth();
-            var ch = $( "#modal-content" ).outerHeight();
+            var cw = $( "#viewchange" ).outerWidth();
+            var ch = $( "#viewchange" ).outerHeight();
 
             //センタリングを実行する
-            $( "#modal-content" ).css( {"left": ((w - cw)/2) + "px","top": ((h - ch)/2) + "px"} ) ;
+            $( "#viewchange" ).css( {"left": ((w - cw)/2) + "px","top": ((h - ch)/2) + "px"} ) ;
 
         }
 
     } ) ;
+		}
+	})
+
 }
 
-//meintest
-//test用localstorage使用の判定
-if (!window.localStorage) {
-    alert("お使いのブラウザはlocalstorageに対応してません。");
+function viewchange(){
+    $(window).bind("resize load",function(){
+        if($(window).height()<$(window).width()){
+            $( "#viewchange,#modal-overlay" ).fadeOut( "slow" , function(){
+
+                        //[#modal-overlay]を削除する
+                        $('#modal-overlay').remove() ;
+                    } ) ;
+        }else{
+        viewhantei();
+        }
+    } ) ;
 }
-localget();
-cleardisp(senkan_1);
-localput();
--- javascript/kaiten_Question.js
+++ javascript/kaiten_Question.js
@@ -25,104 +25,7 @@ function imgkaiten(){
     $("#kaiten").rotate(angle);
     },50);
 }
-var modal_content;
-/*クリアフラグ三つ毎*/ 
-function cleartrue(clearnum){
-    window.localStorage.setItem(clearnum,true);
-}
-//モーダルダイアログの表示
-function modal(mc){
-    //モーダルダイアログに表示させる内容
-    /*
-    switch (modal_content){
-        case 0:
-            /*styleタグ画面上のどこに置くかを指定している 
-            document.getElementById("modal-content").innerHTML="<p style='margin-top:25vh;'><img src='../image/B_K2.png' style='width:40vw; height:25vh;'></p>";
-            document.getElementById("modal-content").innerHTML+="<div class='center'><input type='button' id='modal-close' class='tojiru'></div>";
-            break;
-        case 1:
-            document.getElementById("modal-content").innerHTML="<?={$data['hint_kaiten']}?>";
-            break;
-        case 2:
-            document.getElementById("modal-content").innerHTML="<p style='margin-top:25vh;'><img src='../image/kaiten_clear.png' style='width:40vw; height:8vh;'></p>";
-            document.getElementById("modal-content").innerHTML+="<div class='center'><input type='button' class='mapback' onclick='mapjump()'></div>";
-            break;
-        case 3:
-            document.getElementById("modal-content").innerHTML="<?={$data['gameover_kaiten']}?>";
-    }
-    */
-    $(function(){
 
-    //モーダルウィンドウを出現させるクリックイベント
-    $(function(){
-
-        //キーボード操作などにより、オーバーレイが多重起動するのを防止する
-        $( this ).blur() ;	//ボタンからフォーカスを外す
-        if( $( "#modal-overlay" )[0] ) return false ;		//新しくモーダルウィンドウを起動しない (防止策1)
-        //if($("#modal-overlay")[0]) $("#modal-overlay").remove() ;		//現在のモーダルウィンドウを削除して新しく起動する (防止策2)
-
-        //オーバーレイを出現させる
-        $( "body" ).append( '<div id="modal-overlay"></div>' ) ;
-        $( "#modal-overlay" ).fadeIn( "slow" ) ;
-
-        //コンテンツをセンタリングする
-        centeringModalSyncer() ;
-
-        //コンテンツをフェードインする
-        $( "#modal-content"+mc ).fadeIn( "slow" ) ;
-        if(mc == 2 || mc == 3){
-            //クリックしても閉じない
-            $( "#modal-close" ).unbind().click( function(){
-
-                //[#modal-content]と[#modal-overlay]をフェードアウトした後に…
-                $( "#modal-overlay,#modal-content"+mc ).fadeOut( "slow" , function(){
-
-                    //[#modal-overlay]を削除する
-                    $('#modal-overlay').remove() ;
-                } ) ;
-
-            } ) ;
-        }else{
-            //[#modal-overlay]、または[#modal-close]をクリックしたら…
-            $( "#modal-overlay,#modal-close" ).unbind().click( function(){
-
-                //[#modal-content]と[#modal-overlay]をフェードアウトした後に…
-                $( "#modal-overlay,#modal-content"+mc ).fadeOut( "slow" , function(){
-
-                    //[#modal-overlay]を削除する
-                    $('#modal-overlay').remove() ;
-                } ) ;
-
-            } ) ;
-        }
-
-    } ) ;
-    
-
-    //リサイズされたら、センタリングをする関数[centeringModalSyncer()]を実行する
-    $( window ).resize( centeringModalSyncer ) ;
-
-        //センタリングを実行する関数
-        function centeringModalSyncer() {
-
-            //画面(ウィンドウ)の幅、高さを取得
-            var w = $( window ).width() ;
-            var h = $( window ).height() ;
-
-            // コンテンツ(#modal-content)の幅、高さを取得
-            // jQueryのバージョンによっては、引数[{margin:true}]を指定した時、不具合を起こします。
-    //		var cw = $( "#modal-content" ).outerWidth( {margin:true} );
-    //		var ch = $( "#modal-content" ).outerHeight( {margin:true} );
-            var cw = $( "#modal-content"+mc ).outerWidth();
-            var ch = $( "#modal-content"+mc ).outerHeight();
-
-            //センタリングを実行する
-            $( "#modal-content"+mc ).css( {"left": ((w - cw)/2) + "px","top": ((h - ch)/2) + "px"} ) ;
-
-        }
-
-    } ) ;
-}
 
 //問題のランダム出力
 function questionrand(kakudo){
@@ -130,9 +33,6 @@ function questionrand(kakudo){
     $("#sentaku img").rotate(rand);
 }
 
-function mapjump(){
-    window.location.href = 'map.html';
-}
 function  localput(kaiten){
                                                     //どの難易度をクリアしたのかの判定をつける
     //回転クリアフラグ
@@ -145,80 +45,4 @@ function  localput(kaiten){
     if(kaiten == "kaiten_3"){
     window.localStorage.setItem("kaiten_3",true);
     } 
-}
-/*
-function  localput(){
-    //回転クリアフラグput
-    window.localStorage.setItem("kaiten_3",true); 
-}
-*/
-/*画面の縦横判定共通部分前頁共通*/
-function viewhantei(){
-
-    $(function(){
-		if($(window).height()<$(window).width()){
-			
-		}else{
-			 $(function(){
-
-    //モーダルウィンドウを出現させるクリックイベント
-    $(function(){
-
-        //キーボード操作などにより、オーバーレイが多重起動するのを防止する
-        $( this ).blur() ;	//ボタンからフォーカスを外す
-        if( $( "#modal-overlay" )[0] ) return false ;		//新しくモーダルウィンドウを起動しない (防止策1)
-        //if($("#modal-overlay")[0]) $("#modal-overlay").remove() ;		//現在のモーダルウィンドウを削除して新しく起動する (防止策2)
-
-        //オーバーレイを出現させる
-        $( "body" ).append( '<div id="modal-overlay"></div>' ) ;
-        $( "#modal-overlay" ).fadeIn( "slow" ) ;
-
-        //コンテンツをセンタリングする
-        centeringModalSyncer() ;
-
-        //コンテンツをフェードインする
-        $( "#viewchange" ).fadeIn( "slow" ) ;
-
-    } ) ;
-
-    //リサイズされたら、センタリングをする関数[centeringModalSyncer()]を実行する
-    $( window ).resize( centeringModalSyncer ) ;
-
-        //センタリングを実行する関数
-        function centeringModalSyncer() {
-
-            //画面(ウィンドウ)の幅、高さを取得
-            var w = $( window ).width() ;
-            var h = $( window ).height() ;
-
-            // コンテンツ(#modal-content)の幅、高さを取得
-            // jQueryのバージョンによっては、引数[{margin:true}]を指定した時、不具合を起こします。
-    //		var cw = $( "#modal-content" ).outerWidth( {margin:true} );
-    //		var ch = $( "#modal-content" ).outerHeight( {margin:true} );
-            var cw = $( "#viewchange" ).outerWidth();
-            var ch = $( "#viewchange" ).outerHeight();
-
-            //センタリングを実行する
-            $( "#viewchange" ).css( {"left": ((w - cw)/2) + "px","top": ((h - ch)/2) + "px"} ) ;
-
-        }
-
-    } ) ;
-		}
-	})
-
-}
-
-function viewchange(){
-    $(window).bind("resize load",function(){
-        if($(window).height()<$(window).width()){
-            $( "#viewchange,#modal-overlay" ).fadeOut( "slow" , function(){
-
-                        //[#modal-overlay]を削除する
-                        $('#modal-overlay').remove() ;
-                    } ) ;
-        }else{
-        viewhantei();
-        }
-    } ) ;
 }
