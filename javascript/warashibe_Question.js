c32112e8ac3e0772a733c5de884b9b19b59cc003
kotaseto <150012@yses.ac.jp>
2016-12-19 11:27:30 +0900

    わらしべクリアフラグ
    引数の変更やjs共通部分の削除

--- javascript/warashibe_Question.js
+++ javascript/warashibe_Question.js
@@ -6,11 +6,13 @@ var done;
 var haveitem;
 var wantitem;
 var gitem;
+var mode;
 
-function shoki(mode){
+function shoki(level){
+    mode = level;
     //自分が持っているアイテム
     myitem = 0;
-    if(mode == "easy"){
+    if(mode == 1){
         maxloop = 3;
         //場所:0~7
         place = [0,1,2,3];
@@ -25,7 +27,7 @@ function shoki(mode){
         //目標のアイテム
         gitem = 3;
     }
-    if(mode == "normal"){
+    if(mode == 2){
         maxloop = 5;
         //場所:0~7
         place = [0,1,2,3,4,5];
@@ -40,7 +42,7 @@ function shoki(mode){
         //目標のアイテム
         gitem = 5;
     }
-    if(mode == "hard"){
+    if(mode == 3){
         maxloop = 7;
         //場所:0~7
         place = [0,1,2,3,4,5,6,7];
@@ -139,6 +141,7 @@ function koukan(disp){
                 //自分が持っているアイテムが目標のアイテムなら
                 if(myitem == gitem){
                     //クリアする
+                    cleartrue('warashibe_'+mode);
                     modal(2);
                 }else{
                     //ゲームオーバー判定
@@ -198,20 +201,10 @@ function renewal(){
 
 
 var modal_content;
-/*クリアフラグ三つ毎*/ 
-function cleartrue(clearnum){
-    window.localStorage.setItem(clearnum,true);
-}
 
-//問題のランダム出力
-function questionrand(kakudo){
-    var rand = Math.floor( Math.random() * 4 )*kakudo;
-    $("#sentaku img").rotate(rand);
-}
 
-function mapjump(){
-    window.location.href = 'map.html';
-}
+
+
 function  localput(warashibe){
                                                     //どの難易度をクリアしたのかの判定をつける
     //回転クリアフラグ
@@ -224,80 +217,4 @@ function  localput(warashibe){
     if(warashibe == "warashibe_3"){
     window.localStorage.setItem("warashibe_3",true);
     } 
-}
-/*
-function  localput(){
-    //回転クリアフラグput
-    window.localStorage.setItem("kaiten_3",true); 
-}
-*/
-/*画面の縦横判定共通部分前頁共通*/
-function viewhantei(){
-
-    $(function(){
-		if($(window).height()<$(window).width()){
-			
-		}else{
-			 $(function(){
-
-    //モーダルウィンドウを出現させるクリックイベント
-    $(function(){
-
-        //キーボード操作などにより、オーバーレイが多重起動するのを防止する
-        $( this ).blur() ;	//ボタンからフォーカスを外す
-        if( $( "#modal-overlay" )[0] ) return false ;		//新しくモーダルウィンドウを起動しない (防止策1)
-        //if($("#modal-overlay")[0]) $("#modal-overlay").remove() ;		//現在のモーダルウィンドウを削除して新しく起動する (防止策2)
-
-        //オーバーレイを出現させる
-        $( "body" ).append( '<div id="modal-overlay"></div>' ) ;
-        $( "#modal-overlay" ).fadeIn( "slow" ) ;
-
-        //コンテンツをセンタリングする
-        centeringModalSyncer() ;
-
-        //コンテンツをフェードインする
-        $( "#viewchange" ).fadeIn( "slow" ) ;
-
-    } ) ;
-
-    //リサイズされたら、センタリングをする関数[centeringModalSyncer()]を実行する
-    $( window ).resize( centeringModalSyncer ) ;
-
-        //センタリングを実行する関数
-        function centeringModalSyncer() {
-
-            //画面(ウィンドウ)の幅、高さを取得
-            var w = $( window ).width() ;
-            var h = $( window ).height() ;
-
-            // コンテンツ(#modal-content)の幅、高さを取得
-            // jQueryのバージョンによっては、引数[{margin:true}]を指定した時、不具合を起こします。
-    //		var cw = $( "#modal-content" ).outerWidth( {margin:true} );
-    //		var ch = $( "#modal-content" ).outerHeight( {margin:true} );
-            var cw = $( "#viewchange" ).outerWidth();
-            var ch = $( "#viewchange" ).outerHeight();
-
-            //センタリングを実行する
-            $( "#viewchange" ).css( {"left": ((w - cw)/2) + "px","top": ((h - ch)/2) + "px"} ) ;
-
-        }
-
-    } ) ;
-		}
-	})
-
-}
-
-function viewchange(){
-    $(window).bind("resize load",function(){
-        if($(window).height()<$(window).width()){
-            $( "#viewchange,#modal-overlay" ).fadeOut( "slow" , function(){
-
-                        //[#modal-overlay]を削除する
-                        $('#modal-overlay').remove() ;
-                    } ) ;
-        }else{
-        viewhantei();
-        }
-    } ) ;
 }
