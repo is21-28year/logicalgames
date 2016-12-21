fe2e2be4c1f7b443a2c9ca8d8fdd850cbe619f80
yuuyanakagawa <yse.iss.nakagawa@gmail.com>
2016-12-21 14:35:01 +0900

    船渡し　phpでの問題の難易度別　完了

--- html/funawatashi_Question.php
+++ html/funawatashi_Question.php
@@ -1,3 +1,21 @@
+<?php
+    $nanido = "";
+    $onload;
+    $nanido = $_POST["nanido"];
+    if($nanido=='1'){
+        $onload="'shoki(0);viewhantei();viewchange();modal(0);'";
+    }else if($nanido=='2'){
+        $onload="'shoki(1);viewhantei();viewchange();modal(0);'";
+    }else if($nanido=='3'){
+        $onload="'shoki(2);viewhantei();viewchange();modal(0);'";
+    }
+    //MySQLに接続
+    $con = mysqli_connect("localhost","root","","db_question");
+    //tbl_kaiinからすべてのレコード取得
+    $sql = "select * from tbl_funawatashi where number_funawatashi = '$nanido'" ;
+    $rst = mysqli_query($con,$sql) or die("失敗");
+    $data = mysqli_fetch_array($rst);
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -10,23 +28,21 @@
     <link rel="stylesheet" type="text/css" href="../css/Question.css">
     <link rel="stylesheet" type="text/css" href="../css/funawatashi_Question.css">
 </head>
-<body onload="shoki(2);viewhantei();viewchange();modal(0);">
+<body onload=<?php $onload ?>>
     <!-- ここからモーダルウィンドウ -->
     <div id="modal-content0">
 	    <p style='margin-top:25vh;'><img src='../image/B_F2.png' style='width:40vw; height:30vh;'></p>
         <div class='center'><input type='button' id='modal-close0' class='tojiru'></div>
     </div>
     <div id="modal-content1">
-        <p style='margin-top:25vh;'><img src='../image/H_F2.png' style='width:50vw; height:12vh;'></p>
-        <div class='center'><input type='button' id='modal-close1' class='tojiru'></div>
+        <?= $data['hint_funawatashi'] ?>
     </div>
     <div id="modal-content2">
         <p style='margin-top:25vh;'><img src='../image/funawatashi_clear.png' style='width:40vw; height:8vh;'></p>
         <div class='center'><input type='button' class='mapback' onclick='mapjump()'></div>
     </div>
     <div id="modal-content3">
-        <p style='margin-top:25vh;'><img src='../image/funawatashi_gameover.png' style='width:35vw; height:12.5vh;'></p>
-        <div><input type='button' class='leftbtn mapback'  onclick='mapjump()'><input type='button'  id='modal-close3' class='rightbtn yarinaosi'></div>
+        <?= $data['gameover_funawatashi'] ?>
     </div>
 
     <div id="viewchange">
@@ -45,7 +61,7 @@
     <!--問題文章div-->
     <div id="bunsho" class="b">
     <img id="M_T" src="../image/M_T.png">
-    <img style="width:25vw; height:25vh;" src="../image/funawatashi_bunsho2.png">
+    <?= $data['bunsho_funawatashi'] ?>
     </div>
     
     <!--リセットボタン配置-->
@@ -56,20 +72,7 @@
     
     </div>
     <div class="clam_right">
-        <!--問題動作div-->
-        <div id="lbox" class="box">
-            <div onclick="animeidou('l1')" id="l1"><img src="../image/wolf.png" alt=""></div>
-            <div onclick="animeidou('l2')" id="l2"><img src="../image/sheep.png" alt=""></div>
-            <div onclick="animeidou('l3')" id="l3"><img src="../image/cabbage.png" alt=""></div>
-        </div>
-        <div id="fune" onclick="animefune()">
-            <img src="../image/fune_L.png" alt="">
-        </div>
-        <div id="rbox" class="box">
-            <div onclick="animeidou('r1')" id="r1"><img src="../image/wolf.png" alt=""></div>
-            <div onclick="animeidou('r2')" id="r2"><img src="../image/sheep.png" alt=""></div>
-            <div onclick="animeidou('r3')" id="r3"><img src="../image/cabbage.png" alt=""></div>
-        </div>
+       <?= $data['contents_funawatashi'] ?>
     </div>
 </body>
 </html>
