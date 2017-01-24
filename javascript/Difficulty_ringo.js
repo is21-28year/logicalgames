4d168632a82a51b4ec3e4cea080a1e20cea579c4
yuuyanakagawa <yse.iss.nakagawa@gmail.com>
2017-01-24 10:56:16 +0900

    ビューチェンジ修正

--- javascript/Difficulty_ringo.js
+++ javascript/Difficulty_ringo.js
@@ -122,13 +122,84 @@ function cleardisp(){
 function localdelete(){
     window.localStorage.removeItem("ringo_tutorial");
 } 
+/*画面の縦横判定共通部分前頁共通*/
+function viewhantei(){
 
+    $(function(){
+		if($(window).height()<$(window).width()){
+			
+		}else{
+			 $(function(){
+
+    //モーダルウィンドウを出現させるクリックイベント
+    $(function(){
+
+        //キーボード操作などにより、オーバーレイが多重起動するのを防止する
+        $( this ).blur() ;	//ボタンからフォーカスを外す
+        if( $( "#modal-overlay" )[0] ) return false ;		//新しくモーダルウィンドウを起動しない (防止策1)
+        //if($("#modal-overlay")[0]) $("#modal-overlay").remove() ;		//現在のモーダルウィンドウを削除して新しく起動する (防止策2)
+
+        //オーバーレイを出現させる
+        $( "body" ).append( '<div id="modal-overlay"></div>' ) ;
+        $( "#modal-overlay" ).fadeIn( "slow" ) ;
+
+        //コンテンツをセンタリングする
+        centeringModalSyncer() ;
+
+        //コンテンツをフェードインする
+        $( "#viewchange" ).fadeIn( "slow" ) ;
+
+    } ) ;
+
+    //リサイズされたら、センタリングをする関数[centeringModalSyncer()]を実行する
+    $( window ).resize( centeringModalSyncer ) ;
+
+        //センタリングを実行する関数
+        function centeringModalSyncer() {
+
+            //画面(ウィンドウ)の幅、高さを取得
+            var w = $( window ).width() ;
+            var h = $( window ).height() ;
+
+            // コンテンツ(#modal-content)の幅、高さを取得
+            // jQueryのバージョンによっては、引数[{margin:true}]を指定した時、不具合を起こします。
+    //		var cw = $( "#modal-content" ).outerWidth( {margin:true} );
+    //		var ch = $( "#modal-content" ).outerHeight( {margin:true} );
+            var cw = $( "#viewchange" ).outerWidth();
+            var ch = $( "#viewchange" ).outerHeight();
+
+            //センタリングを実行する
+            $( "#viewchange" ).css( {"left": ((w - cw)/2) + "px","top": ((h - ch)/2) + "px"} ) ;
+
+        }
+
+    } ) ;
+		}
+	})
+
+}
+
+function viewchange(){
+    $(window).bind("resize load",function(){
+        if($(window).height()<$(window).width()){
+            $( "#viewchange,#modal-overlay" ).fadeOut( "slow" , function(){
+
+                        //[#modal-overlay]を削除する
+                        $('#modal-overlay').remove() ;
+                    } ) ;
+        }else{
+        viewhantei();
+        }
+    } ) ;
+}
 //メイン動作部分に変更
 function main(){
     localget();
     cleardisp();
     tutorialhantei();
     localput();
+    viewhantei();
+    viewchange();
 }
 
 /*更新日：10/26
